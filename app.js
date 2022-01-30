34e84e019445a38f1fbcea99a1cad82273a4e2b6
akhilbhadrangirija <83879587+akhilbhadrangirija@users.noreply.github.com>
2022-01-30 15:41:27 +0530

    Mongodb added and created database

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var db=require('./configuration/connection')
 var fileUpload=require('express-fileupload')
 
 var usersRouter = require('./routes/users');
@@ -24,7 +24,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+db.connect((err)=>{
+  if(err){
+    console.log("database error");
 
+  }
+  else{
+    console.log("Database connected");
+  }
+})
 app.use('/', usersRouter);
 app.use('/admin', adminRouter);
 
